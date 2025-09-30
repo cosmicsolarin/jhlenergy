6cd98599909ed70a162ce8bdde384fcc8a62e14c
cosmicsolarin <prajyu4pes@gmail.com>
2025-10-01 00:04:42 +0530

    Added ui edits on testmonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -45,8 +45,8 @@ const Testimonials = () => {
   };
 
   return (
-    <section className="py-16 sm:py-24 text-black">
-      <div className="container mx-auto px-4 bg-green-50 p-4">
+    <section className="py-16 sm:py-24 text-black bg-green-50">
+      <div className="container mx-auto px-4  p-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Left Column */}
           <div className="space-y-6">
